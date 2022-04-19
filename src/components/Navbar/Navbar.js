7a289aea8a590658f3f1ef7ece449fa768b35edb
Dan <daniel.liemng@gmail.com>
2022-04-19 03:22:57 -0400

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.scss';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import { images } from '../../constants';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toogleMenu, setToogleMenu] = useState(false);
+
+  return (
+    <div className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.gericht} alt='logo' />
+      </div>
+
+      <ul className='app__navbar-links'>
+        <li className='p__opensans' id='links'>
+          <a href='#home'>Home</a>
+        </li>
+        <li className='p__opensans' id='links'>
+          <a href='#about'>About</a>
+        </li>
+        <li className='p__opensans'>
+          <a href='#menu'>Menu</a>
+        </li>
+        <li className='p__opensans'>
+          <a href='#awards'>Awards</a>
+        </li>
+        <li className='p__opensans'>
+          <a href='#contact'>Contact</a>
+        </li>
+      </ul>
+
+      <div className='app__navbar-login'>
+        <a href='#login' className='p__opensans'>
+          Login / Register
+        </a>
+        <div></div>
+        <a href='/' className='p__opensans'>
+          Book Table
+        </a>
+      </div>
+
+      <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu
+          color='#fff'
+          fontSize={27}
+          onClick={() => setToogleMenu(true)}
+        />
+
+        {toogleMenu && (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className='overlay__close'
+              onClick={() => setToogleMenu(false)}
+            />
+
+            <ul className='app__navbar-smallscreen-links'>
+              <li className='p__opensans' onClick={() => setToogleMenu(false)}>
+                <a href='#home'>Home</a>
+              </li>
+              <li className='p__opensans' onClick={() => setToogleMenu(false)}>
+                <a href='#about'>About</a>
+              </li>
+              <li className='p__opensans' onClick={() => setToogleMenu(false)}>
+                <a href='#menu'>Menu</a>
+              </li>
+              <li className='p__opensans' onClick={() => setToogleMenu(false)}>
+                <a href='#awards'>Awards</a>
+              </li>
+              <li className='p__opensans' onClick={() => setToogleMenu(false)}>
+                <a href='#contact'>Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
